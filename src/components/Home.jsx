3d8bdac33cc93513754c293e02fc6adf98c90c41
AkivaKn <akivakaufman@gmail.com>
2024-10-15 14:13:09 +0100

    feat: show payment trends for each product
    Install recharts and add a line graph for each product with more than 1 years data, showing the payment and benchmark trends

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,15 @@ import {
   convertCurrencyToEuros,
   showProductPaymentTrends,
 } from "../utils";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export default function Home() {
   const [providerTotals, setProviderTotals] = useState([]);
@@ -22,38 +31,89 @@ export default function Home() {
       const productPaymentTrends = showProductPaymentTrends(
         productBenchmarksInEuros
       );
+      console.log(productPaymentTrends);
       setProductTrends(productPaymentTrends);
     };
     fetchBenchmarks();
   }, []);
+
+  const yAxisFormatter = (value) =>
+    `${new Intl.NumberFormat("en-US", {
+      notation: "compact",
+      compactDisplay: "short",
+    }).format(value)}`;
   return (
     <>
       <h1>Payment Data</h1>
-      <h2>Totals by Provider</h2>
-      <ul>
-        {providerTotals.map((provider, index) => {
-          return (
-            <li key={index}>
-              <h3>{provider.provider_name}</h3>
-              <p>
-                Payment: 
-                {new Intl.NumberFormat('en-UK', {
-                  style: "currency",
-                  currency: "EUR",
-                }).format(provider.total_payment)}
-              </p>
-              <p>Benchmark: {new Intl.NumberFormat('en-UK', {
-                  style: "currency",
-                  currency: "EUR",
-              }).format(provider.total_benchmark)}</p>
-                  <>Benchmark difference: {new Intl.NumberFormat('en-UK', {
-                  style: "currency",
-                  currency: "EUR",
-              }).format(provider.total_benchmark - provider.total_payment)}</>
-            </li>
-          );
-        })}
-      </ul>
+      <section>
+        <h2>Totals by Provider</h2>
+        <ul>
+          {providerTotals.map((provider, index) => {
+            return (
+              <li key={index}>
+                <h3>{provider.provider_name}</h3>
+                <p>
+                  Payment:
+                  {new Intl.NumberFormat("en-UK", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(provider.total_payment)}
+                </p>
+                <p>
+                  Benchmark:{" "}
+                  {new Intl.NumberFormat("en-UK", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(provider.total_benchmark)}
+                </p>
+                <>
+                  Benchmark difference:{" "}
+                  {new Intl.NumberFormat("en-UK", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(provider.total_benchmark - provider.total_payment)}
+                </>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section>
+        <h2>Payment Trends by Product</h2>
+        <ul>
+          {productTrends.map((product, index) => {
+            return (
+              <li key={index}>
+                <h3>{product.product_name}</h3>
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={product.product_trends}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 60,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="year" />
+                  <YAxis tickFormatter={yAxisFormatter} />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="payment"
+                    stroke="#8884d8"
+                    activeDot={{ r: 8 }}
+                  />
+                  <Line type="monotone" dataKey="benchmark" stroke="#82ca9d" />
+                </LineChart>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </>
   );
 }
