47c00a4e63aab53842a728c2935e65c50f498a67
AkivaKn <akivakaufman@gmail.com>
2024-10-15 13:30:51 +0100

    feat: create util functions to modify data
    Create functions to convert currency to Euro, calculate total payments and benchmarks by provider and sort the data into products to show payment trends.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import { getProductBenchmarks } from "../data";
+import {
+  calculateTotalsByProvider,
+  convertCurrencyToEuros,
+  showProductPaymentTrends,
+} from "../utils";
+
 export default function Home() {
-    return <h1>Home page</h1>
+  const [providerTotals, setProviderTotals] = useState([]);
+  const [productTrends, setProductTrends] = useState([]);
+  useEffect(() => {
+    const fetchBenchmarks = async () => {
+      const productBenchmarks = await getProductBenchmarks();
+      const productBenchmarksInEuros = await convertCurrencyToEuros(
+        productBenchmarks
+      );
+      const calculatedTotals = calculateTotalsByProvider(
+        productBenchmarksInEuros
+      );
+      setProviderTotals(calculatedTotals);
+      const productPaymentTrends = showProductPaymentTrends(
+        productBenchmarksInEuros
+      );
+      setProductTrends(productPaymentTrends);
+    };
+    fetchBenchmarks();
+  }, []);
+  return <h1>Home page</h1>;
 }
---
+++ src/utils.js
@@ -0,0 +1,70 @@
+import { getExchangeRates } from "./data";
+
+
+export async function convertCurrencyToEuros(productBenchmarksData) {
+    const productBenchmarksWithYearKey = []
+    productBenchmarksData.forEach((product) => {
+        const productWithYear = { ...product };
+        productWithYear.year = new Date(product.start_date).getFullYear()
+        productBenchmarksWithYearKey.push(productWithYear);
+    });
+    const exchangeRates = await getExchangeRates();
+    const productBenchmarksInEuros = productBenchmarksWithYearKey.map((product) => {
+    if (product.currency.id === 3) {
+        return product;
+    } else {
+        const conversionRate = exchangeRates.filter((exchangeRate) => {
+            return (
+                exchangeRate.from_currency_id === product.currency.id &&
+                exchangeRate.to_currency_id === 3 &&
+                exchangeRate.year === product.year
+            );
+        });
+        const convertedPayment = product.payment * conversionRate[0].exchange_rate;
+        const convertedBenchmark = product.benchmark * conversionRate[0].exchange_rate;
+        product.payment = convertedPayment;
+        product.benchmark = convertedBenchmark;
+        product.currency = { id: 3, name: "EUR", symbol: "€" };
+        return product;
+    }
+    });
+    return productBenchmarksInEuros;
+}
+
+function categoriseBenchmarks(productBenchmarks,key) {
+    const categorisedBenchmarks = {};
+    productBenchmarks.forEach((product) => {
+        categorisedBenchmarks[product[key]] =
+        categorisedBenchmarks[product[key]] || [];
+        categorisedBenchmarks[product[key]].push(product);
+    });
+    return categorisedBenchmarks
+}
+
+export function calculateTotalsByProvider(productBenchmarks) {
+    const benchmarksByProvider = categoriseBenchmarks(productBenchmarks, 'provider_name');
+    const totalsByProvider = [];
+    for (const provider in benchmarksByProvider) {
+        let totalPayment = 0;
+        let totalBenchmark = 0;
+        benchmarksByProvider[provider].forEach((product) => {
+            totalPayment += product.payment;
+            totalBenchmark += product.benchmark;
+        })
+        totalsByProvider.push({
+            provider_name: provider,
+            totalPayment,totalBenchmark
+        })
+    }
+    return totalsByProvider;
+}
+export function showProductPaymentTrends(productBenchmarks) {
+    const benchmarksByProduct = categoriseBenchmarks(productBenchmarks, 'product_name');
+    const paymentTrends = [];
+    for (const product in benchmarksByProduct) {
+        if (benchmarksByProduct[product].length > 1) {
+            paymentTrends.push({product_name:product,product_trends:benchmarksByProduct[product]})
+        }
+    }
+    return paymentTrends;
+}
